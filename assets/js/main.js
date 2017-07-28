541a998698fc493632d9a0ba9515db71b93e0d84
Paulina Aros <aros.pau@gmail.com>
2017-07-28 14:06:51 -0400

    imprimiendo json

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 for (var i = 0 ; i<20; i++){
 		console.log(pinterest[i].title);
-		$(".listafotos").append()
+		$(".listafotos").append("<div class='listafotitos'> <img src='dist/img/" + pinterest[i].image_url + "'></div>");
+		console.log(pinterest[i].description);
+		$(".listafotos").append("<h4 class='listafotitos'>" + pinterest[i].description + "</h4>");
 	}
